912282b6e80d282cc87eb29dc968ac30bc346c76
Avneesh-shrivastava <shrivastavavneesh4122005@gmail.com>
2026-04-27 23:25:38 +0530

    Added password and confirm password validation

--- edu/views.py
+++ edu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.contrib import messages
 
 # Create your views here.
 def student_login(request):
@@ -9,25 +10,37 @@ def student_signup(request):
     if request.method == 'POST':
         data = request.POST
 
+        global st_username
+        global st_password
+
         st_name = data.get('fullname')
         st_email = data.get('email')
         st_username = data.get('username')
         st_password = data.get('password')
-        st_confirm_pass = data.get('fullname')
+        st_confirm_pass = data.get('confirm_password')
 
-        student_signup_data.objects.create(
-            st_name = st_name,
-            st_email = st_email,
-            st_username = st_username,
-            st_password = st_password,
-            st_confirm_pass = st_confirm_pass
-        )
+        if st_password == st_confirm_pass:
+            student_signup_data.objects.create(
+                st_name = st_name,
+                st_email = st_email,
+                st_username = st_username,
+                st_password = st_password,
+                st_confirm_pass = st_confirm_pass
+                )
 
-        view_data = student_signup_data.objects.all().values()
-        print(view_data)
+            view_data = student_signup_data.objects.all().values()
+            print(view_data)
+
+        if st_password != st_confirm_pass:
+            messages.error(request, "Password did not matched")
+            print(st_password)
+            print(st_confirm_pass)
+            return render(request, 'signup.html')
+
+        print(st_password)
+        print(st_confirm_pass)
+        
         return redirect('/student-login/')
-    
-
     return render(request, 'signup.html')
 
 def show_data(request):
@@ -41,4 +54,9 @@ def delete_data(request, id):
     return redirect('/show-data/')
 
 def student_login(request):
+    if request.method == 'POST':
+        data = request.POST
+        log_username = data.get('username')
+        log_password = data.get('password')
+        
     return render(request, 'login.html')
