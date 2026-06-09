9a3905fa242ea309ce68e25ecb5ed5dac5895ddd
Avneesh-shrivastava <shrivastavavneesh4122005@gmail.com>
2026-06-09 12:32:09 +0530

    if user exists

--- edu/views.py
+++ edu/views.py
@@ -11,15 +11,21 @@ def student_signup(request):
     if request.method == 'POST':
         data = request.POST
 
-        global st_username
-        global st_password
-
         st_name = data.get('fullname')
         st_email = data.get('email')
         st_username = data.get('username')
         st_password = data.get('password')
         st_confirm_pass = data.get('confirm_password')
+        
+        user = User.objects.filter(username = st_username)
 
+        if user.exists():
+            messages.error(request, "This username already exists")
+            return redirect('/student-signup/')
+            
+        if st_password != st_confirm_pass:
+            messages.error(request, "Password did not matched")
+            return render(request, 'signup.html')
 
         user = User.objects.create(
             first_name = st_name,
@@ -30,30 +36,20 @@ def student_signup(request):
         user.set_password(st_password)
         user.save()
 
-        view_data = student_signup_data.objects.all().values()
+        view_data = User.objects.all().values()
         print(view_data)
-
-        if st_password != st_confirm_pass:
-            messages.error(request, "Password did not matched")
-            return render(request, 'signup.html')
-        
-        username_column = student_signup_data.objects.values('st_username')
-
-        if {'st_username':f'{st_username}'} in username_column :
-            messages.error(request, "This username already exist's")
-            return render(request, 'signup.html')
-
 
         return redirect('/student-login/')
     return render(request, 'signup.html')
 
 def show_data(request):
-    queryset = student_signup_data.objects.all()
+    queryset = User.objects.all()
     context = {'students_data' : queryset}
+
     return render(request, 'show_data.html', context)
 
 def delete_data(request, id):
-    that_data = student_signup_data.objects.get(id=id)
+    that_data = User.objects.get(id=id)
     that_data.delete()
     return redirect('/show-data/')
 
