49e41bf49be224cab6cb04ba84154658bf75d2e5
Avneesh-shrivastava <shrivastavavneesh4122005@gmail.com>
2026-06-16 12:21:39 +0530

    updated enrollment form

--- core/settings.py
+++ core/settings.py
@@ -138,4 +138,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# SESSION_COOKIE_AGE = 1209600          # 2 weeks in seconds
+# SESSION_EXPIRE_AT_BROWSER_CLOSE = False
+# SESSION_SAVE_EVERY_REQUEST = True
 
