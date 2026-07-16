a31a5e79cced4bc3b64db2b2fa3432279ba34572
avneeshshrivastava <shrivastavavneesh4122005@gmail.com>
2026-07-16 12:17:54 +0530

    Deleted extra STATICFIELS_DIR

--- core/settings.py
+++ core/settings.py
@@ -123,14 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 
+
+
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "public/static")]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIR = {
-    os.path.join(BASE_DIR, "public/static")
-}
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/static')
 MEDIA_URL = '/media/'
